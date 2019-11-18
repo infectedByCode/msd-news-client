3866af74200aa4d91cb6005764cec9192d4e7913
Matt Daley <mdaleyhk@gmail.com>
2019-11-18 10:17:50 +0000

    Added Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from '@reach/router'
+
+const Header = () => {
+  return (
+    <nav>
+      <Link to='/'>Home</Link>
+    </nav>
+  )
+}
+
+export default Header
