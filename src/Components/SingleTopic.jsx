3f1c358cb70b51e46444509f62c8007ecb57785f
Matt Daley <mdaleyhk@gmail.com>
2019-11-18 15:12:30 +0000

    Change from class to functional component.

--- src/Components/SingleTopic.jsx
+++ src/Components/SingleTopic.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from 'react'
-import * as api from '../api'
 import ArticleList from './ArticleList'
 
-class SingleTopic extends Component {
-  render () {
-    const { topic } = this.props
+const SingleTopic = props => {
+  const { topic } = props
 
-    return (
-      <main>
-        <ArticleList topic={topic} />
-      </main>
-    )
-  }
+  return (
+    <main>
+      <ArticleList topic={topic} />
+    </main>
+  )
 }
 
 export default SingleTopic
