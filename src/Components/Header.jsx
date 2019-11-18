0b2e422cb767ca431cd31f87f61bdc587ec7efea
Matt Daley <mdaleyhk@gmail.com>
2019-11-18 21:36:38 +0000

    Add basic styling and grid layout to navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,12 @@ const Header = () => {
   return (
     <header>
       <nav>
+        <Link id='logo' to='/'>
+          newsBits
+        </Link>
         <Link to='/'>Home</Link>
+        <Link to='/'>#User</Link>
+        <Link to='/'>#login</Link>
       </nav>
       <h1>Welcome to NewsBits</h1>
     </header>
