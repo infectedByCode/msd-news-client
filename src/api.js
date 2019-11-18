d055507841400a048db583292b8c5b94a8bccae4
Matt Daley <mdaleyhk@gmail.com>
2019-11-18 15:47:12 +0000

    Add fetch article by ID and comments by article ID for SingleArticle component

--- src/api.js
+++ src/api.js
@@ -7,6 +7,18 @@ export const getArticles = async topic => {
   return data.articles
 }
 
+export const getArticleById = async id => {
+  const { data } = await axios.get(`${baseUrl}/articles/${id}`)
+
+  return data.article
+}
+
+export const getCommentsByArticleId = async id => {
+  const { data } = await axios.get(`${baseUrl}/articles/${id}/comments`)
+
+  return data.comments
+}
+
 export const getTopics = async () => {
   const { data } = await axios.get(`${baseUrl}/topics`)
   return data.topics
