e50dd9b7bc705bcae59b32bdee7847b3aa8b2349
Matt Daley <mdaleyhk@gmail.com>
2019-11-25 09:22:20 +0000

    Add more users to login.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -36,6 +36,10 @@ class Header extends Component {
                   </option>}
                 <option value="weegembump">weegembump</option>
                 <option value="jessjelly">jessjelly</option>
+                <option value="happyamy2016">happyamy2016</option>
+                <option value="grumpy19">grumpy19</option>
+                <option value="tickle122">tickle122</option>
+                <option value="cooljmessy">cooljmessy</option>
               </select>}
             <button>
               {loggedIn ? 'Logout' : 'Login'}
